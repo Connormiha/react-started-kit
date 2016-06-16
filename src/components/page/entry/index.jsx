c3633299f08f6cb14963e5f48d07c3b4d0b530ee
Mihail Bodrov <connormiha1@gmail.com>
2016-06-17 00:10:19 +0300

    :art: add page pure, static class methods

--- src/components/page/entry/index.jsx
+++ src/components/page/entry/index.jsx
@@ -1,12 +1,8 @@
-const b = bem.default('entry');
-
 import {connect} from 'react-redux';
 import * as userActions from 'flux/user';
-import WorkSheetForm from 'components/form/WorkSheet';
-import WorkSheetTable from 'components/common/WorkSheetTable';
-import './index.styl';
+import PageEntryPure from './index.pure';
 
-@connect(
+export default connect(
     ({user}) => ({user}),
     (dispatch) => {
         return {
@@ -23,40 +19,4 @@ import './index.styl';
             }
         };
     }
-)
-export default class PageEntry extends React.Component {
-    handleChangeName({target: {value}}) {
-        this.props.onUpdateUserName(value);
-    }
-
-    handleChangeSurname({target: {value}}) {
-        this.props.onUpdateUserSurname(value);
-    }
-
-    handleChangeSex({target: {value}}) {
-        this.props.onUpdateUserSex(value);
-    }
-
-    render() {
-        let {user} = this.props;
-
-        return (
-            <div className={b}>
-                <h2>Hello {user.name || 'Anonym'}</h2>
-                <div className={b('worksheet')}>
-                    <div className={b('form')}>
-                        <WorkSheetForm
-                            {...user}
-                            onChangeName={this.handleChangeName.bind(this)}
-                            onChangeSurname={this.handleChangeSurname.bind(this)}
-                            onChangeSex={this.handleChangeSex.bind(this)}
-                        />
-                    </div>
-                    <div className={b('preview')}>
-                        <WorkSheetTable {...user} />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+)(PageEntryPure);
---
+++ src/components/page/entry/index.pure.jsx
@@ -0,0 +1,42 @@
+const b = bem.default('entry');
+
+import WorkSheetForm from 'components/form/WorkSheet';
+import WorkSheetTable from 'components/common/WorkSheetTable';
+import './index.styl';
+
+export default class PageEntryPure extends React.Component {
+    handleChangeName({target: {value}}) {
+        this.props.onUpdateUserName(value);
+    }
+
+    handleChangeSurname({target: {value}}) {
+        this.props.onUpdateUserSurname(value);
+    }
+
+    handleChangeSex({target: {value}}) {
+        this.props.onUpdateUserSex(value);
+    }
+
+    render() {
+        let {user} = this.props;
+
+        return (
+            <div className={b}>
+                <h2>Hello {user.name || 'Anonym'}</h2>
+                <div className={b('worksheet')}>
+                    <div className={b('form')}>
+                        <WorkSheetForm
+                            {...user}
+                            onChangeName={this.handleChangeName.bind(this)}
+                            onChangeSurname={this.handleChangeSurname.bind(this)}
+                            onChangeSex={this.handleChangeSex.bind(this)}
+                        />
+                    </div>
+                    <div className={b('preview')}>
+                        <WorkSheetTable {...user} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
